aa84aa05aa590773fe9ebb6bdea4b947f7cc5835
miftt <miftahfauzi012@gmail.com>
2023-12-06 22:29:13 +0700

    add: api product and fetching

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -3,13 +3,15 @@ import { NextRequest, NextResponse } from "next/server";
 const data = [
     {
         id: 1,
-        name: "Sepatu Baru",
-        price: 100000,
+        title: "Nike Dunk Retro",
+        price: 1700000,
+        image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/a3e7dead-1ad2-4c40-996d-93ebc9df0fca/dunk-low-retro-shoe-66RGqF.png"
     },
     {
         id: 2,
-        name: "Sepatu Baru 2",
-        price: 10000000,
+        title: "Nike Air Force 1",
+        price: 1400000,
+        image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e6da41fa-1be4-4ce5-b89c-22be4f1f02d4/air-force-1-07-shoes-WrLlWX.png"
     }
 ];
 
