10707b9d1ea20e34d6d6988f118145d99e2af12e
miftt <miftahfauzi012@gmail.com>
2023-12-10 06:37:06 +0700

    add: middleware config

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+    const isLogin = false;
+    if(!isLogin){
+        return NextResponse.redirect(new URL("/login", request.url))
+    }
+}
+
+export const config = {
+    matcher: [
+        "/dashboards/:path*",
+        // "/api/:path*"
+    ]
+}
