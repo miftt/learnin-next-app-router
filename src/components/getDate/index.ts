4e33d142720cb64da14eba9b9b5a3c6a9058121e
miftt <miftahfauzi012@gmail.com>
2023-12-09 10:36:04 +0700

    fix: docs for getDate() return object

--- src/components/getDate/index.ts
+++ src/components/getDate/index.ts
@@ -1,3 +1,9 @@
+/**
+ * Fungsi ini mengembalikan tanggal dan waktu saat ini dalam format "HH:MM:SS WIB - DD/MM/YYYY".
+ * 
+ * @returns {string} String yang berisi tanggal dan waktu saat ini.
+ */
+
 const getDate = () => {
     let date = new Date();
     let waktu = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
@@ -5,7 +11,14 @@ const getDate = () => {
     let bulan = date.getMonth() + 1; // Bulan dimulai dari 0, jadi kita tambahkan 1.
     let tahun = date.getFullYear();
 
-    return `${waktu} WIB - ${tanggal}/${bulan}/${tahun}`;
+    let allData = `${waktu} WIB - ${tanggal}/${bulan}/${tahun}`;
+
+    return {
+        allData: allData,
+        waktu: waktu,
+        tanggal: tanggal,
+        bulan: bulan,
+        tahun: tahun
+    };
 }
- 
 export default getDate;
