e5f18b1a51eca8e2fefbfab70e86db8690bca064
miftt <miftahfauzi012@gmail.com>
2023-12-10 06:17:23 +0700

    tidy up little bit

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,48 +1,57 @@
 import { NextRequest, NextResponse } from "next/server";
 
 const data = [
-    {
-        id: 1,
-        title: "Nike Dunk Retro",
-        price: 1700000,
-        image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/a3e7dead-1ad2-4c40-996d-93ebc9df0fca/dunk-low-retro-shoe-66RGqF.png"
-    },
-    {
-        id: 2,
-        title: "Nike Air Force 1",
-        price: 1400000,
-        image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e6da41fa-1be4-4ce5-b89c-22be4f1f02d4/air-force-1-07-shoes-WrLlWX.png"
-    },
-    {
-        id: 3,
-        title: "Nike Air Force 1",
-        price: 1400000,
-        image: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e6da41fa-1be4-4ce5-b89c-22be4f1f02d4/air-force-1-07-shoes-WrLlWX.png"
-    },
-    
+  {
+    id: 1,
+    title: "Nike Dunk Retro",
+    price: 1700000,
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/a3e7dead-1ad2-4c40-996d-93ebc9df0fca/dunk-low-retro-shoe-66RGqF.png",
+  },
+  {
+    id: 2,
+    title: "Nike Dunk Retro",
+    price: 1700000,
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/a3e7dead-1ad2-4c40-996d-93ebc9df0fca/dunk-low-retro-shoe-66RGqF.png",
+  },
+  {
+    id: 3,
+    title: "Nike Air Force 1",
+    price: 1400000,
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e6da41fa-1be4-4ce5-b89c-22be4f1f02d4/air-force-1-07-shoes-WrLlWX.png",
+  },
+  {
+    id: 4,
+    title: "Nike Air Force 1",
+    price: 1400000,
+    image:
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e6da41fa-1be4-4ce5-b89c-22be4f1f02d4/air-force-1-07-shoes-WrLlWX.png",
+  },
 ];
 
 export async function GET(request: NextRequest) {
-    const {searchParams} = new URL(request.url);
-    const id = searchParams.get("id");
-    if (id) {
-        const detailProduct = data.find((item) => item.id === Number(id))
-        if (detailProduct) {
-            return NextResponse.json({
-                status: 200,
-                message: "Success",
-                data: detailProduct
-            })
-        }
-        return NextResponse.json({
-            status: 404,
-            message: "Not Found",
-            data: {}
-        })
-    }
-    return NextResponse.json({
+  const { searchParams } = new URL(request.url);
+  const id = searchParams.get("id");
+  if (id) {
+    const detailProduct = data.find((item) => item.id === Number(id));
+    if (detailProduct) {
+      return NextResponse.json({
         status: 200,
         message: "Success",
-        data: data
-    })
+        data: detailProduct,
+      });
+    }
+    return NextResponse.json({
+      status: 404,
+      message: "Not Found",
+      data: {},
+    });
+  }
+  return NextResponse.json({
+    status: 200,
+    message: "Success",
+    data: data,
+  });
 }
--- src/app/products/[[...slug]]/page.tsx
+++ src/app/products/[[...slug]]/page.tsx
@@ -1,72 +1,73 @@
 type ProductPageProps = {
-    params: {
-        slug: string;
-    }
+  params: {
+    slug: string;
+  };
+};
+
+async function getData() {
+  //   const res = await fetch("https://fakestoreapi.com/products", {
+  //     cache: "no-store",
+  //   });
+  const res = await fetch("http://localhost:3000/api/product", {
+    cache: "no-store",
+    // next: {
+    //   tags: ["products"],
+    //   // revalidate: 15,
+    // },
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
 }
 
-async function getData(){
-    // const res = await fetch('https://fakestoreapi.com/products');
-    const res = await fetch('http://localhost:3000/api/product',{
-        cache: 'force-cache',
-        next: {
-            tags: ['products'],
-            // revalidate: 15,
-        }
-    });
+const ProductPage = async (props: ProductPageProps) => {
+  const { params } = props;
+  const products = await getData();
+  return (
+    <div className="grid grid-cols-4 m-5 place-items-center">
+      {/* <h1>{params.slug ? "Detail Product Page" : "Product Page"}</h1> */}
+      {products.data.length > 0 &&
+        products.data.map((products: any) => (
+          <div
+            key={products.id}
+            className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-5"
+          >
+            <img
+              className="p-8 rounded-t-lg object-cover h-96 w-full"
+              src={products.image}
+              alt="product image"
+            />
+            <div className="px-5 pb-5">
+              <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white truncate">
+                {products.title}
+              </h5>
+              <div className="flex items-center justify-between mt-3">
+                <span className="text-3xl font-bold text-gray-900 dark:text-white">
+                  ${products.price}
+                </span>
+                <a
+                  href="#"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Add to cart
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
 
-    if(!res.ok){
-        throw new Error ('Failed to fetch data');
-    }
+      {params.slug && (
+        <>
+          <p>Category: {params.slug[0]} </p>
+          <p>Gender: {params.slug[1]} </p>
+          <p>Id: {params.slug[2]} </p>
+        </>
+      )}
+    </div>
+  );
+};
 
-    return res.json();
-}
-
-const ProductPage = async ( props:
-    ProductPageProps,
-) => {
-    const { params } = props;
-    const products = await getData();
-    console.log(products);
-    return ( 
-        <div className="grid grid-cols-4 m-5 place-items-center">
-            {/* <h1>{params.slug ? "Detail Product Page" : "Product Page"}</h1> */}
-            {products.data.length > 0 && 
-                products.data.map((products: any) => (
-                        <div key={products.id} className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-5">
-                            <img
-                                className="p-8 rounded-t-lg object-cover h-96 w-full"
-                                src={products.image}
-                                alt="product image"
-                            />
-                            <div className="px-5 pb-5">
-                                <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white truncate">
-                                {products.title}
-                                </h5>
-                                <div className="flex items-center justify-between mt-3">
-                                    <span className="text-3xl font-bold text-gray-900 dark:text-white">
-                                    ${products.price}
-                                    </span>
-                                    <a
-                                        href="#"
-                                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                                    >
-                                        Add to cart
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                )
-            )}
-
-            {params.slug && (
-                <>
-                 <p>Category: {params.slug[0]} </p>
-                 <p>Gender: {params.slug[1]} </p>
-                 <p>Id: {params.slug[2]} </p>
-                </>
-            )}
-        </div>
-     );
-}
- 
 export default ProductPage;
