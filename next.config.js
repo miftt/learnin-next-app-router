91cb3d06a04b1aa2835d84de6f2102ad8c9e3fa9
miftt <miftahfauzi012@gmail.com>
2023-12-10 08:01:12 +0700

    add: routes api image for (Image Next Components)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images:{
+       domains: ['static.nike.com', 'fakestoreapi.com']
+    }
+}
 
 module.exports = nextConfig
