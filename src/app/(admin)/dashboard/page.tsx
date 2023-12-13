eb2d01e867ab57e1183bdf36be4450cd17e82e1b
miftt <miftahfauzi012@gmail.com>
2023-12-13 20:49:28 +0700

    fix: client render

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,21 +1,6 @@
-"use client"
-
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+'use client'
 
 const DashboardPage = () => {
-    const {data: session, status}: {data: any; status:string;} = useSession();
-    const router = useRouter();
-    useEffect(() => {
-        if (status === "unauthenticated") {
-            router.push("/login");
-        } else {
-            if(session !== undefined && session?.user.role !== 'admin') {
-                router.push("/");
-            }
-        }
-    },[router, session, session?.user.role, status])
     return ( 
         <div 
             className="
