c5ad852bbc7e93c154b806bfebda0b22190c7b2b
miftt <miftahfauzi012@gmail.com>
2023-12-13 20:28:17 +0700

    add: multirole

---
+++ src/middlewares/withAuth.ts
@@ -0,0 +1,31 @@
+import { getToken } from "next-auth/jwt";
+import { NextFetchEvent, NextMiddleware, NextRequest, NextResponse } from "next/server";
+
+const onlyAdminPage = ['/dashboard']
+
+const withAuth = (
+    middleware: NextMiddleware,
+    requireAuth: string[] = [],
+) => {
+    return async (req: NextRequest, next: NextFetchEvent) => {
+        const pathname = req.nextUrl.pathname;
+
+        if(requireAuth.includes(pathname)){
+            const token = await getToken({
+                req,
+                secret: process.env.NEXTAUTH_SECRET
+            });
+            if(!token){
+                const url = new URL('/login', req.url);
+                url.searchParams.set('callbackUrl', encodeURI(req.url));// CALLBACK MISAL SAAT BUKA DASHBOARD MAKA BALIK KE DASHBOARD JIKA SUDAH LOGIN
+                return NextResponse.redirect(url);
+            }
+            if(token.role !== 'admin' && onlyAdminPage.includes(pathname)){
+                return NextResponse.redirect(new URL('/', req.url));
+            }
+        }
+        return middleware(req, next);
+    };
+}
+ 
+export default withAuth;
